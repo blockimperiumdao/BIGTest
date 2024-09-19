3067329b9cbd714da91873909457ecb5a0ecba7a
blockimperiumdao <gpierce@blockimperium.xyz>
2024-09-19 14:40:08 -0400

    Refactor of the project to BIGTest. Added fucntionality to test ERC721 and ERC1155

--- ui.cs
+++ ui.cs
@@ -1,6 +1,6 @@
 using Godot;
 using System;
-using BIGConnect.addons.godotblockchain;
+using GodotBlockchain.addons.godotblockchain;
 
 public partial class ui : CanvasLayer
 {
@@ -8,6 +8,12 @@ public partial class ui : CanvasLayer
 	
 	[Export]
 	private ERC20BlockchainContractNode				erc20ContractNode;
+	
+	[Export]
+	private ERC1155BlockchainContractNode			erc1155ContractNode;
+	
+	[Export]
+	private ERC721BlockchainContractNode			erc721ContractNode;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -45,7 +51,7 @@ public partial class ui : CanvasLayer
 	
 	public async void _on_claim_erc20_button_pressed()
 	{
-		GD.Print("Pressed");
+		GD.Print("ERC20 Pressed");
 		
 		erc20ContractNode.Initialize();
 		
@@ -62,6 +68,51 @@ public partial class ui : CanvasLayer
 			GD.Print("No contract node");
 		}
 	}
+	
+	public async void _on_claim_erc721_button_pressed()
+	{
+		GD.Print("ERC721 claim Pressed");
+		
+		erc721ContractNode.Initialize();
+		
+		if (erc721ContractNode != null)
+		{
+			ERC721BlockchainContractNode.ERC721TokenMetadata metadata = await erc721ContractNode.FetchMetadata();
+			
+			GD.Print(metadata.TokenName);
+			
+			await erc721ContractNode.Claim( 1 );
+			
+			//await erc20ContractNode.Claim("1.0");
+		}
+		else
+		{
+			GD.Print("No contract node");
+		}
+	}
+	
+	public async void _on_claim_erc1155_button_pressed()
+	{
+		GD.Print("ERC1155 claim Pressed");
+		
+		int tokenId = 4;
+		int quantity = 1;
+		
+		erc1155ContractNode.Initialize();
+		
+		if (erc1155ContractNode != null)
+		{
+			ERC1155BlockchainContractNode.ERC1155TokenMetadata metadata = await erc1155ContractNode.FetchMetadataForToken(tokenId);
+			
+			GD.Print(metadata.TokenName);
+			
+			await erc1155ContractNode.Claim( tokenId, quantity );
+		}
+		else
+		{
+			GD.Print("No contract node");
+		}
+	}	
 	
 	public void DisplayLog( string message )
 	{
