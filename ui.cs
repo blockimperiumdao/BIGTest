b5a8c746cac692053c27a2e39807e05aa5980241
blockimperiumdao <gpierce@blockimperium.xyz>
2024-09-20 21:02:20 -0400

    Making some changes to the resources and the scene to support testing ERC721 and ERC1155 contract claiming.

--- ui.cs
+++ ui.cs
@@ -53,15 +53,17 @@ public partial class ui : CanvasLayer
 	{
 		GD.Print("ERC20 Pressed");
 		
-		erc20ContractNode.Initialize();
-		
 		if (erc20ContractNode != null)
 		{
+			erc20ContractNode.Initialize();
+			
 			ERC20BlockchainContractNode.ERC20TokenMetadata metadata = await erc20ContractNode.FetchMetadata();
 			
 			GD.Print(metadata.TokenName);
 			
 			await erc20ContractNode.Claim("1.0");
+			
+			GD.Print("ERC20 Claimed!");
 		}
 		else
 		{
@@ -73,10 +75,10 @@ public partial class ui : CanvasLayer
 	{
 		GD.Print("ERC721 claim Pressed");
 		
-		erc721ContractNode.Initialize();
-		
 		if (erc721ContractNode != null)
 		{
+			erc721ContractNode.Initialize();
+
 			ERC721BlockchainContractNode.ERC721TokenMetadata metadata = await erc721ContractNode.FetchMetadata();
 			
 			GD.Print(metadata.TokenName);
@@ -84,10 +86,12 @@ public partial class ui : CanvasLayer
 			await erc721ContractNode.Claim( 1 );
 			
 			//await erc20ContractNode.Claim("1.0");
+			
+			GD.Print("ERC721 Claimed!");
 		}
 		else
 		{
-			GD.Print("No contract node");
+			GD.Print("No contract node - did you remember to set the contract node in the Godot UI?");
 		}
 	}
 	
@@ -98,19 +102,21 @@ public partial class ui : CanvasLayer
 		int tokenId = 4;
 		int quantity = 1;
 		
-		erc1155ContractNode.Initialize();
-		
 		if (erc1155ContractNode != null)
 		{
+			erc1155ContractNode.Initialize();
+
 			ERC1155BlockchainContractNode.ERC1155TokenMetadata metadata = await erc1155ContractNode.FetchMetadataForToken(tokenId);
 			
 			GD.Print(metadata.TokenName);
 			
 			await erc1155ContractNode.Claim( tokenId, quantity );
+			
+			GD.Print("ERC1155 Claimed!");
 		}
 		else
 		{
-			GD.Print("No contract node");
+			GD.Print("No contract node - did you remember to set the ERC1155 contract node in the Godot UI?");
 		}
 	}	
 	
